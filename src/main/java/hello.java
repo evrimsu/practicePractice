4e6ce64c9d439adea43706feed0537c5edf2ec16
evrimeylem <evrimeylemo@gmail.com>
2021-02-18 13:36:04 +0000

    eu is learning

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,6 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        System.out.println("b21");
+        System.out.println("b21 was good");
+        System.out.println("i think b22 is better");
 
     }
 }
