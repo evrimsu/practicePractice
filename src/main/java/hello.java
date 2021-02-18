7298ec25be3e8ddfa35d4cefcfbf3cd0fd7d1602
evrimeylem <evrimeylemo@gmail.com>
2021-02-18 19:15:57 +0000

    master branch spouse change the vovie location

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,10 +1,12 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hello word, b22");
         System.out.println("b21");
         System.out.println("b21 was good");
         System.out.println("i think b22 is better");
         System.out.println("ohhh need to add more comment");
+        //more movie
+
 
     }
 }
